e3a1c286ba2c4988f884ee92a7e75c04a0fde5c7
Cristian Abrante <alu0100945850@ull.edu.es>
2018-11-04 16:23:09 +0000

    Add: main function implemented

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,11 +4,29 @@ import PRF.Pow;
 import org.javatuples.Pair;
 import org.javatuples.Tuple;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-	  // write your code here
+    int x, y;
+    switch (args.length) {
+      case 0:
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("introduce x > ");
+        x = Integer.parseInt(scanner.nextLine());
+        System.out.println("introduce y > ");
+        y = Integer.parseInt(scanner.nextLine());
+        break;
+      case 2:
+        x = Integer.parseInt(args[0]);
+        y = Integer.parseInt(args[1]);
+        break;
+        default:
+          throw new IllegalArgumentException("wrong number of arguments");
+    }
+
     Pow pow = new Pow();
-    Tuple t = pow.call(Pair.with(3,3));
-    System.out.println(t.getValue(0));
+    Integer result = (Integer) pow.call(Pair.with(x, y)).getValue(0);
+    System.out.printf("pow(%d, %d) = %d", x, y, result);
   }
 }
